ddbce1d52594b4e57aea9a50f3fbb9c2335e43f9
Ronnapat <rburanakate@palo-it.com>
2022-08-11 15:08:56 +0700

    s

--- dojo-2/solutions/exercise_1_ronnapat.sh
+++ dojo-2/solutions/exercise_1_ronnapat.sh
@@ -1,1 +1,1 @@
-grep -o devops dojo-2/example_ronnapat.txt | wc -l
+grep -o devops dojo-2/example.txt | wc -l
--- dojo-2/solutions/exercise_2_ronnapat.sh
+++ dojo-2/solutions/exercise_2_ronnapat.sh
@@ -1,1 +1,1 @@
-sed 's/devops/hello_word/g' dojo-2/example.txt > dojo-2/solutions/new_example.txt
+sed 's/devops/hello_word/g' dojo-2/example.txt > dojo-2/new_example.txt
